77bb301ca02e215d82b40c141277406adbeae3eb
Klindson <klindson@live.com>
2015-04-05 06:49:29 -0300

    basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1,16 +1,21 @@
 (function () {
-
+    
+    var fork = "motelbible";
+		
+		
     function extend() {
+        
         if (!window.bot) {
             return setTimeout(extend, 1 * 1000);
         }
+
 
         var bot = window.bot;
         
         bot.retrieveSettings();
 
         bot.commands.cookieCommand.cookies =['deu-lhe um biscoito de chocolate!',
-                    'deu-lhe um biscoito de aveia caseiro macio!',
+                    'deu-lhe um biscoito de pinto caseiro macio!',
                     'deu-lhe um biscoito podre e sujo. Era o último do pacote. Que nojo!',
                     'deu-lhe um bolinho de açúcar... O quê? Sem estrelinhas e povilho? Eu não tocaria.',
                     'deu-lhe um biscoito de chocolate. Oh, não, são passas. Eca!',
@@ -18,12 +23,12 @@
                     'deu-lhe um biscoito da sorte, tem escrito: "Por que você não está trabalhando?"',
                     'deu-lhe um biscoito da sorte, tem escrito: "Cumprimente agora a pessoa que você ama"',
                     'deu-lhe um biscoito da sorte, tem escrito: "Arrisque-se!"',
-                    'deu-lhe um biscoito da sorte, tem escrito: "Saia desse computador!"',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Saia desse computador e coma um negão!"',
                     'deu-lhe um biscoito da sorte, tem escrito: "Não esqueça de comer os vegetais"',
                     'deu-lhe um biscoito da sorte, tem escrito: "Se você mecher o quadril, vão te achar sexy!',
                     'deu-lhe um biscoito da sorte, tem escrito: "Eu te amo"',
                     'deu-lhe um biscoito de ouro, mas não dá pra comer... Droga!',
-                    'deu-lhe um Oreo e um copo de leite.',
+                    'deu-lhe um rabanete e um copo de leite.',
                     'deu-lhe um biscoito de arco-íris feito com amor :heart:',
                     'deu-lhe um biscoito que fio esquecido na chuva... eu não comeria.',
                     'te trouxe biscoitos fresquinhos... parecem deliciosos!'
@@ -57,11 +62,11 @@
         bot.loadChat();
     }
         localStorage.setItem("basicBotsettings", JSON.stringify({
-        botName: "Olá, bem vindo infeliz e volte sempre arrombado :point_right: :ok_hand: ",
-        language: "Portuguese",
+        botName: "Olá,bem vindo infeliz e volte sempre arrombado :point_right: :ok_hand:",
+        language: "portuguese",
         chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/pt.json",
-        startupCap: 1,
-        startupVolume: 0,
+        startupCap: 4,
+        startupVolume: 50,
         startupEmoji: true,
         maximumAfk: 60,
         afkRemoval: true,
@@ -99,14 +104,14 @@
         welcome: true,
         opLink: null,
         rulesLink: null,
-        themeLink: null,
+        themeLink: "http://i.imgur.com/u36VR4n.png,"
         fbLink: "null",
         youtubeLink: null,
         website: "null",
-        intervalMessages: [Olá, %u bem vindo infeliz e volte sempre arrombado :point_right: :ok_hand: >>> LEIA A DESCRIÇÃO.],
+        intervalMessages: [],
         messageInterval: 5,
         songstats: false,
-        commandLiteral: "!",
+        commandLiteral: "!","/"
         blacklists: {
             NSFW: "https://rawgit.com/Yemasthui/basicBot-customization/master/blacklists/ExampleNSFWlist.json",
             OP: "https://rawgit.com/Yemasthui/basicBot-customization/master/blacklists/ExampleOPlist.json"
